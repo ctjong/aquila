29f7727d3b10e4b755220089341c3d5d6dec7ff3
ctjong <christophertjong@hotmail.com>
2017-07-26 00:58:22 -0700

    enrolled plan parser

--- app/src/main/java/com/projectaquila/datamodels/PlanCollection.java
+++ app/src/main/java/com/projectaquila/datamodels/PlanCollection.java
@@ -5,6 +5,7 @@ import com.projectaquila.common.PlanCollectionType;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.List;
 
@@ -37,11 +38,18 @@ public class PlanCollection extends CollectionModelBase<Plan> {
             JSONArray plans = (JSONArray)plansObj;
             for(int i=0; i<plans.length(); i++){
                 try {
-                    Plan plan = Plan.parse(plans.get(i));
-                    if(plan != null)
+                    Object planJson = plans.get(i);
+                    if(mType == PlanCollectionType.ENROLLED && planJson instanceof JSONObject){
+                        planJson = ((JSONObject)planJson).getJSONObject("plan");
+                    }
+                    Plan plan = Plan.parse(planJson);
+                    if(plan != null){
                         getItems().add(plan);
+                    }else{
+                        System.err.println("[PlanCollectionAdapter.processServerResponse] failed to parse plan, null found");
+                    }
                 }catch(JSONException e){
-                    System.err.println("[PlanCollectionAdapter.processServerResponse] an error occurred while trying to get plans at index " + i);
+                    System.err.println("[PlanCollectionAdapter.processServerResponse] an error occurred while trying to get plan at index " + i);
                     e.printStackTrace();
                 }
             }
